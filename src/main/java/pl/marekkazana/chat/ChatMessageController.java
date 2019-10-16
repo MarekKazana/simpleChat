5790110998b6dd1f945e80ef78d59d0f37640222
Marek Kazana <m.kazana@interia.pl>
2019-10-16 22:51:32 +0200

    Basic functions chat, separating messages and users

---
+++ src/main/java/pl/marekkazana/chat/ChatMessageController.java
@@ -0,0 +1,17 @@
+package pl.marekkazana.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatMessageController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+
+
+    public ChatMessage get(ChatMessage chatMessage){
+        return chatMessage;
+    }
+}
